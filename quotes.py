b874e167a5f6898c58f10b108589120d70b8ce2a
gsblinkhorn <gsblinkhorn@gmail.com>
2018-04-05 16:53:24 -0400

    Fixed error in img class

--- quotes.py
+++ quotes.py
@@ -7,7 +7,7 @@ url = 'https://www.brainyquote.com/quote_of_the_day'
 def get_quote():
     cont = requests.get(url).content
     soup = BeautifulSoup(cont, 'html.parser')
-    image = soup.find('img', {'class': " p-qotd bqPhotoDefault bqPhotoDefaultFw img-responsive"})
+    image = soup.find('img', {'class': "p-qotd bqPhotoDefault bqPhotoDefaultFw img-responsive"})
 
     return(str(image.get('alt', '')))
 
