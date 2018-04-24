ce5fa4aeec86cfdaced2f57d3f4dc2a47222cd86
gsblinkhorn <gsblinkhorn@gmail.com>
2018-04-24 13:23:29 -0400

    Eliminate quotes module -> function now within email_html

--- email_html.py
+++ email_html.py
@@ -1,8 +1,21 @@
-import quotes
+import requests
+from bs4 import BeautifulSoup
 
+quote_url = 'https://www.brainyquote.com/quote_of_the_day'
+
+# Retrieves Daily Quote
+def get_quote():
+    cont = requests.get(quote_url).content
+    soup = BeautifulSoup(cont, 'html.parser')
+    image = soup.find('img', {'class': "p-qotd bqPhotoDefault bqPhotoDefaultFw img-responsive"})
+
+    return(str(image.get('alt', '')))
+
+
+# Generates body of Email Message
 def generate_html(image_html_list, title_list, author_list, top_com_list):
     print('Generating HTML')
-    quote_source = "<a href=" +"\"" + quotes.url + "\"" + ">BrainyQuote - Quote of the Day</a>"
+    quote_url_html = "<a href=" +"\"" + quote_url + "\"" + ">BrainyQuote - Quote of the Day</a>"
 
     # Email Header HTML
     code = """
@@ -12,8 +25,8 @@ def generate_html(image_html_list, title_list, author_list, top_com_list):
         <p align="center"><b><font size="7">The Feline Fanatic</b></font><br>
         <p align="center"><i><font size="4">"Your Daily Dose of All Things Fluffball"</i></font><br><br>
         <hr>
-        <p align="center"><i><font size="5">""" + quotes.get_quote() + """</i></font><br>
-        <p align="center"><font size="1">""" + quote_source + """</p></font>
+        <p align="center"><i><font size="5">""" + get_quote() + """</i></font><br>
+        <p align="center"><font size="1">""" + quote_url_html + """</p></font>
         <hr><br>
     """
     # Image HTML Generator 
